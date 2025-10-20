e3a4d0158a70daa55863ebdc90e4fccce22c38f4
Ilan Iwumbwe <ikibfm@gmail.com>
2025-10-20 15:10:06 +0100

    proof from lecture 1

--- isabelle/HSV_lecture1.thy
+++ isabelle/HSV_lecture1.thy
@@ -1,35 +1,36 @@
-theory HSV_lecture1 imports Complex_Main begin
+theory HSV_lecture1 imports Complex_Main
+begin
 
-find_theorems "_ \<in> \<rat>" 
+(*
+  hence: following from the previous statement, this statement holds
+  when we need to go to some arbitrary place in the proof, we can refer to it by name
+*)
+
+find_theorems "_ \<in> \<rat>"
 thm Rats_abs_nat_div_natE
 
-theorem sqrt2_irrational: "sqrt 2 \<notin> \<rat>"
-proof 
+theorem "sqrt 2 \<notin> \<rat>"
+proof
   assume "sqrt 2 \<in> \<rat>"
-  then obtain m n
-    where "\<bar>sqrt 2\<bar> = real m / real n" and coprimemn: "coprime m n" and john: "n \<noteq> 0"
-    by (rule Rats_abs_nat_div_natE)
-  hence "2 = (real m / real n)^2"
-    by (metis abs_of_nat of_nat_numeral real_sqrt_abs real_sqrt_power)
-  hence "2 = (real m)^2 / (real n)^2"
-    by (simp add: power_divide)
+  then obtain m n where "n\<noteq>0" and "\<bar>sqrt 2\<bar> = real m / real n" and ** : "coprime m n" using
+  Rats_abs_nat_div_natE by metis
+  hence "\<bar>sqrt 2\<bar>^2 = (real m)^2 / (real n)^2" 
+    using power_divide by auto
+  hence "2 = (real m)^2 / (real n)^2" 
+    by auto
   hence "2 * (real n)^2 = (real m)^2"
-    by (simp add: john eq_divide_eq_numeral(1))
-  hence "2 * n^2 = m^2" 
-    by (metis (mono_tags, lifting) of_nat_mult 
-      of_nat_numeral of_nat_power_eq_of_nat_cancel_iff)
+    by (metis div_by_0 nonzero_eq_divide_eq zero_neq_numeral)
+  hence *: "2*(n^2) = m^2"
+    using of_nat_eq_iff by fastforce
   hence "even (m^2)"
-    by presburger 
-  hence meven: "even m"
-    by simp
-  then obtain m' where "m = 2 * m'" by blast
-  hence "2 * n^2 = (2 * m')^2"
-    by (simp add: \<open>2 * n\<^sup>2 = m\<^sup>2\<close>)
-  hence "2 * n^2 = 4 * m'^2" by simp
-  from this have "n^2 = 2 * m'^2" by simp
-  hence "even (n^2)" by auto
-  hence neven: "even n" by simp
-  from neven and meven and coprimemn show False by auto
+    by presburger
+  hence ***:"even m" by simp
+  then obtain m' where "m = 2*m'" by blast
+  hence "2*(n^2) = (2*m')^2" using * by blast
+  hence "n^2 = 2*(m'^2)" by simp
+  hence "even (n^2)" by presburger
+  hence "even n" by simp  
+  thus False using ** *** by auto
 qed
 
 end
